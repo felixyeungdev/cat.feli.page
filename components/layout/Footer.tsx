85a1f5127d944e09b2dd453f0e3bef9f46065e1d
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-09-08 00:52:39 +0800

    styles

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,17 +3,19 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="flex justify-center py-8 bg-white ring-2 ring-gray-200">
-            <span>
-                Copyright © 2021{" "}
-                <a
-                    href="https://felixyeung.com"
-                    target="_blank"
-                    className="transition-colors hover:text-indigo-600"
-                >
-                    Felix Yeung
-                </a>
-                . All rights reserved.
-            </span>
+            <div className="max-w-[80rem] md:mx-8 mx-6 flex justify-center w-full">
+                <div>
+                    Copyright © 2021{" "}
+                    <a
+                        href="https://felixyeung.com"
+                        target="_blank"
+                        className="transition-colors hover:text-indigo-600"
+                    >
+                        Felix Yeung
+                    </a>
+                    . All rights reserved.
+                </div>
+            </div>
         </div>
     );
 };
