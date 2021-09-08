462fe527d6f64e1849b3b404b79c20766e9f5f7f
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-09-08 17:49:23 +0800

    added back timeline

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import BlueToWhite from "components/design/curves/BlueToWhite";
 import PurpleToBlue from "components/design/curves/PurpleToBlue";
 import WhiteToBlue from "components/design/curves/WhiteToBlue";
 import MeetTheCats from "components/pages/home/meetTheCats/MeetTheCats";
-// import ViewTimeline from "components/pages/home/viewTimeline/ViewTimeline";
+import ViewTimeline from "components/pages/home/viewTimeline/ViewTimeline";
 import React from "react";
 
 const HomePage = () => {
@@ -12,7 +12,7 @@ const HomePage = () => {
             <WhiteToBlue />
             <MeetTheCats />
             <PurpleToBlue />
-            {/* <ViewTimeline /> */}
+            <ViewTimeline />
             <BlueToWhite />
             <div className="flex justify-center py-16 bg-white">
                 <div className="max-w-[80rem] w-full mx-6 md:mx-10">
