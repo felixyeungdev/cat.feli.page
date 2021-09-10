899be4a06ced3a6a93d70055f5120fa0b6fd2511
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-09-11 02:47:45 +0800

    changed credits styles and added credits link

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,19 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
     return (
         <div className="flex justify-center py-8 bg-white ring-2 ring-gray-200">
             <div className="max-w-[80rem] md:mx-8 mx-6 flex justify-center w-full">
-                <div>
-                    Copyright © 2021{" "}
-                    <a
-                        href="https://felixyeung.com"
-                        target="_blank"
-                        className="transition-colors hover:text-indigo-600"
-                    >
-                        Felix Yeung
-                    </a>
-                    . All rights reserved.
+                <div className="text-xs">
+                    <div>
+                        Copyright © 2021{" "}
+                        <a
+                            href="https://felixyeung.com"
+                            target="_blank"
+                            className="transition-colors hover:text-indigo-600"
+                        >
+                            Felix Yeung
+                        </a>
+                        . All rights reserved.
+                    </div>
+                    <div>
+                        <Link href="/credits">
+                            <a className="transition-colors hover:text-indigo-600">
+                                Site Credits
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
