d933d2862f7a68588bab2959f9f095c41903a2fd
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-09-11 22:59:02 +0800

    added loading indicator

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,13 +1,37 @@
 import PageHead from "components/common/PageHead";
 import { AnimateSharedLayout } from "framer-motion";
-import React from "react";
+import { useRouter } from "next/dist/client/router";
+import React, { useEffect, useState } from "react";
 import AppBar from "./AppBar";
 import Footer from "./Footer";
+import Loading from "./Loading";
 
 const Layout: React.FC = ({ children }) => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        let routeChangeStart = () => {
+            setLoading(true);
+        };
+        let routeChangeComplete = () => {
+            setLoading(false);
+        };
+
+        router.events.on("routeChangeStart", routeChangeStart);
+        router.events.on("routeChangeComplete", routeChangeComplete);
+        router.events.on("routeChangeError", routeChangeComplete);
+        return () => {
+            router.events.off("routeChangeStart", routeChangeStart);
+            router.events.off("routeChangeComplete", routeChangeComplete);
+            router.events.off("routeChangeError", routeChangeComplete);
+        };
+    }, []);
+
     return (
         <AnimateSharedLayout>
             <PageHead />
+            <Loading isLoading={loading} />
             <AppBar />
             <main className="min-h-screen">
                 <div>{children}</div>
---
+++ components/layout/Loading.tsx
@@ -0,0 +1,47 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
+import { useWindowSize } from "react-use";
+
+interface Props {
+    isLoading: boolean;
+}
+
+const Loading = ({ isLoading }: Props) => {
+    const { width } = useWindowSize();
+    // return <></>;
+    return (
+        <AnimatePresence>
+            {isLoading && (
+                <motion.div
+                    className="fixed inset-x-0 top-0 h-1 overflow-hidden scale-x-125 bg-opacity-25 pointer-events-none bg-feli"
+                    style={{ zIndex: 999, maxWidth: width }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    layout
+                >
+                    <motion.div
+                        initial={{ paddingLeft: 0, paddingRight: 0 }}
+                        animate={{
+                            paddingLeft: [0, width / 4, width],
+                            paddingRight: [width, width / 4, 0],
+                        }}
+                        transition={{
+                            duration: 2,
+                            repeat: Infinity,
+                            repeatType: "loop",
+                            ease: "linear",
+                        }}
+                    >
+                        <div
+                            className="w-full bg-purple-600 bg-gradient-to-r from-blue-600 to"
+                            style={{ height: 3 }}
+                        ></div>
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
+};
+
+export default Loading;
