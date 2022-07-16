671391db8c5795126c98e2a747f196fbe7e8c50a
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2022-07-16 19:54:24 +0800

    upgraded packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    darkMode: false,
-    mode: "jit",
     theme: {
         fontFamily: { display: ["Inter"] },
         extend: {},
