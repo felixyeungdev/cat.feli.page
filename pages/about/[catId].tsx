e0cd7bd05c62725a6d72cbeee143f36c24a35f40
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2022-07-16 19:49:56 +0800

    uses url query

---
+++ pages/about/[catId].tsx
@@ -0,0 +1,40 @@
+import CatPage from "components/pages/cat/CatPage";
+import cats, { CatData } from "data/cats";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import React from "react";
+
+interface Props {
+    cat: CatData;
+}
+
+const SesamePage: NextPage<Props> = ({ cat }) => {
+    return (
+        <>
+            <CatPage cat={cat} />
+        </>
+    );
+};
+
+export default SesamePage;
+
+export const getStaticPaths: GetStaticPaths = async (context) => {
+    return {
+        paths: Object.keys(cats).map((catId) => ({
+            params: { catId },
+        })),
+        fallback: false,
+    };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+    const catId = context.params.catId?.toString();
+
+    const cat = cats[catId];
+
+    if (!cat)
+        return {
+            notFound: true,
+        };
+
+    return { props: { cat } };
+};
