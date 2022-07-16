4496320bac3ea47948d2c9388154c314b286e712
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2022-07-16 19:54:37 +0800

    updated year

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className="max-w-[80rem] md:mx-8 mx-6 flex justify-center w-full">
                 <div className="text-xs">
                     <div>
-                        Copyright © 2021{" "}
+                        Copyright © 2022{" "}
                         <a
                             href="https://felixyeung.com"
                             target="_blank"
