1cef7e5d7a242f6525c4388ab10899d379c96aaf
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-05-05 02:26:58 +0100

    fix: remove old comment

--- src/env.mjs
+++ src/env.mjs
@@ -3,19 +3,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-/**
- * Specify your client-side environment variables schema here.
- * This way you can ensure the app isn't built with invalid env vars.
- * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
- */
-
-/**
- * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
- * middlewares) or client-side so we need to destruct manually.
- *
- * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
- */
-
 export const env = createEnv({
     server: {
         SANITY_API_READ_TOKEN: z.string().min(1),
