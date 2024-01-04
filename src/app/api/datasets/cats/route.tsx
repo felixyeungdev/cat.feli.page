400315f8091470c0c84ad9724147d7fc288a7452
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2024-01-04 09:33:59 +0000

    add route handler for algolia search

---
+++ src/app/api/datasets/cats/route.tsx
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { getAllCats } from "~/lib/cms/queries";
+
+// modified from https://www.sanity.io/schemas/portable-text-to-plain-text-cc845843
+function blocksToText(blocks: any) {
+    return blocks
+        .map((block) => {
+            if (block._type !== "block" || !block.children) return "";
+            return block.children.map((child) => child.text).join("");
+        })
+        .join("\n\n");
+}
+
+export const GET = async (request: Request) => {
+    const cats = await getAllCats();
+
+    return NextResponse.json(
+        cats.map((cat) => {
+            return {
+                objectID: cat.slug,
+                id: cat.slug,
+                avatar_url: cat.avatar.url,
+                favourite_toys: cat.favouriteToys.map((toy) => toy.name),
+                gender: cat.gender,
+                adopted: cat.adopted,
+                biography: blocksToText(cat.biography),
+                date_of_birth: cat.dateOfBirth,
+                date_of_death: cat.dateOfDeath,
+            };
+        }),
+    );
+};
